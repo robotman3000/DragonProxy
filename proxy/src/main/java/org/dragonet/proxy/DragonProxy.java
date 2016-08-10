d5d9836266cc226e4854eb876341192b93a747dc
TheDiamondYT1 <goldowl37@gmail.com>
2016-08-10 15:47:53 +0100

    Enable metrics for pre-releases - we want to know who is using our software!

--- proxy/src/main/java/org/dragonet/proxy/DragonProxy.java
+++ proxy/src/main/java/org/dragonet/proxy/DragonProxy.java
@@ -104,9 +104,6 @@ public class DragonProxy {
         console = new ConsoleManager(this);
         console.startConsole();
 
-        logger.info(Terminal.GREEN + "\n\n              DRAGONPROXY MULTI MAVEN MODULE TEST\n   " +
-                "This is not ready for production yet and may not ever be ;)\n");
-
 	    //Put at the top instead
 	    if(!IS_RELEASE) {
 	    	logger.warning(Terminal.YELLOW + "This is a development build. It may contain bugs. Do not use on production.\n");
@@ -144,12 +141,12 @@ public class DragonProxy {
 	    //Init session and command stuff
         sessionRegister = new SessionRegister(this);
         commandRegister = new CommandRegister(this);
-		
-        if (IS_RELEASE) {
-            try {
-                metrics = new ServerMetrics(this);
-                metrics.start();
-            } catch (IOException ex) { }
+
+        try {
+            metrics = new ServerMetrics(this);
+            metrics.start();
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
 
         //Create thread pool
